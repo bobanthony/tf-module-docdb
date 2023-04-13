e018f02397cda1b0b6a6e45dd5df02afae23c423
Dinesh <satyadineshk328@gmail.com>
2023-04-13 16:08:44 +0530

    updating ./tf-module-docdb

---
+++ data.tf
@@ -0,0 +1,10 @@
+data "aws_ssm_parameter" "user" {
+  name = "${var.env}.docdb.user"
+}
+
+data "aws_ssm_parameter" "pass" {
+  name = "${var.env}.docdb.pass"
+}
+data "aws_kms_key" "key" {
+  key_id = "alias/roboshop"
+}
